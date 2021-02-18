685c3eb418151d2645471d54d9a632758a6aa94b
sabin ducon <sabinchacko03>
2021-02-18 12:45:51 +0400

    msg

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
-    res.json({message: "Hello from Sabin -> Jephy"});
+    res.json({message: "Hello from Sabin !!!"});
 });
 
 app.get('*', (req, res) => {
